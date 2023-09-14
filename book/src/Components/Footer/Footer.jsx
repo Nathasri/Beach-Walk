8569c347f17e53deb8e9a15d7e4bc72ea66bfe41
Nathasri <nathasri17@gmail.com>
2023-09-14 17:04:48 +0530

    Added

---
+++ book/src/Components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Footer() {
+
+
+  return (
+    <footer className="footer">
+      <h2>Beach Walk | Hotel Room Book</h2>
+      <p>
+       
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.facebook.com/#"
+        >
+          
+        </a>
+      </p>
+    </footer>
+  );
+}
